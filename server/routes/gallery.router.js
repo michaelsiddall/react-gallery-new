66a91d64ac5276198e5aa19b61e4c45c567e1658
Mike Siddall <eatingartist@gmail.com>
2020-09-19 07:08:33 -0500

    added photos to gallery and set up template componant

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -8,13 +8,25 @@ const galleryItems = require('../modules/gallery.data');
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
+    for (const galleryItem of galleryItems) {
+        if (galleryItem.id == galleryId) {
             galleryItem.likes += 1;
         }
     }
     res.sendStatus(200);
 }); // END PUT Route
+
+
+router.get('/:id', (req, res) => {
+    const galleryId = req.params.id;
+    for (const galleryItem of galleryItems) {
+        if (galleryItem.id == galleryId) {
+            // what do we want to do
+            res.send(galleryItem)
+        }
+    }
+    res.sendStatus(404)
+})
 
 // GET Route
 router.get('/', (req, res) => {
---
+++ src/components/_template/_template.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class _template extends Component {
+    componentDidMount() {
+        console.log('_template mounted');
+    } // end componentDidMount
+
+    render() {
+        return (
+            <div>
+                <h1>_template</h1>
+            </div>
+        ); // end return
+    } // end render
+} // end class
+
+export default _template;
