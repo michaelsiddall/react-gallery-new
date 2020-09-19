8691164f4a8ecaa357661745cc9f2f9b8d6d524b
Mike Siddall <eatingartist@gmail.com>
2020-09-19 07:27:15 -0500

    GET in App.js is retrieving gallery images

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,41 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log("App is ready to do things");
+    this.getPicts();
+  }
+
+  getPicts = () => {
+    Axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+      .then((response) => {
+        console.log("response", response);
+        console.log("response.data", response.data);
+
+        this.setState({
+          gallery: response.data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
+
       </div>
     );
   }
